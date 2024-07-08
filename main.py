57212347ae06a32eba5998c9ca108871102ebbbf
Eliana Larregola <eblarregola2012@gmail.com>
2024-07-08 15:13:49 -0300

    EDA y Modelo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = FastAPI()
 
@@ -111,6 +113,60 @@ def get_director(nombre_director: str):
         "director": nombre_director,
         "peliculas": film_details
     }
+
+# Modelo de recomendación
+
+# Función con mi modelo de recomendación
+
+@app.get("/recomendacion/{title}")
+def recomendacion(title: str):
+    try:
+        # Cargar el DataFrame
+        df = pd.read_parquet('streaming_model.parquet')
+
+        # Verificar si el título existe en el DataFrame
+        if title not in df["title"].values:
+            return {"error": "Este título no se encuentra"}
+
+        # Crear una instancia de TfidfVectorizer con las stopwords en inglés
+        tfidf = TfidfVectorizer(stop_words='english')
+
+        # Crear la matriz tf-idf con los features de las películas
+        tfidf_matrix = tfidf.fit_transform(df["genres"] + " " + df["overview"])
+
+        # Obtener el índice del title ingresado
+        idx = df[df['title'] == title].index[0]
+
+        # Validar que el índice esté dentro de los límites de la matriz tfidf_matrix
+        if idx >= tfidf_matrix.shape[0]:
+            return {"error": "Índice fuera de rango en la matriz tf-idf"}
+
+        # Obtener el vector tf-idf del title ingresado
+        item_tfidf_vector = tfidf_matrix[idx]
+
+        # Calcular la matriz de similitud de coseno entre el título ingresado y los demás
+        cosine_sim_matrix = cosine_similarity(item_tfidf_vector, tfidf_matrix)
+
+        # Guardar los scores de similitud en una lista de tuplas, donde el primer elemento es el índice y el segundo es el score
+        sim_scores = [(i, score) for i, score in enumerate(cosine_sim_matrix[0]) if i != idx]
+
+        # Ordenar los scores de mayor a menor
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+        # Obtener los 5 ítems más similares
+        sim_scores = sim_scores[:5]
+
+        # Obtener los títulos de los ítems recomendados y convertirlos en lista
+        recommended_movies = df['title'].iloc[[i[0] for i in sim_scores]].tolist()
+
+        result = {"Películas recomendadas": recommended_movies}
+
+        # Devolver las películas recomendadas
+        return result
+
+    except Exception as e:
+        return {"error": str(e)}
+
 
 
 if __name__ == "__main__":
